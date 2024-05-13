40e30974a8f46ab8606ed86f3f7084c658c100c6
Neel Andhole <neelandhole@Neels-Air.new.rr.com>
2024-05-13 14:45:43 -0500

    added shows

--- Frontend.java
+++ Frontend.java
@@ -1,15 +1,18 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Frontend extends Application {
 
   @Override
-  public void start(Stage arg0) throws Exception {
-    BorderPane Pane = new BorderPane();
+  public void start(Stage stage) throws Exception {
+    BorderPane pane = new BorderPane();
 
-
-
+    Scene scene = new Scene(pane, 800, 600);
+    stage.setScene(scene);
+    stage.setTitle("JavaFXGame");
+    stage.show();
   }
 
 }
